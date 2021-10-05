7ec8e7d742c60328360a4f263be6448bade7dcbf
ihor-tokaryk <ihor_tokaryk@epam.com>
2021-10-05 11:28:04 +0300

    messages

--- app/controllers/messenger/chats_controller.rb
+++ app/controllers/messenger/chats_controller.rb
@@ -5,16 +5,9 @@ require_dependency 'messenger/application_controller'
 module Messenger
   # ChatsController
   class ChatsController < ApplicationController
-    before_action :set_chat, only: :show
-
     def index
       @chats = Chat.all_for(current_user)
       @users_without_chats = User.all_except(current_user).where.not(id: @chats.map { |chat| chat.users.first.id })
-    end
-
-    def show
-      @message = Message.new
-      @messages = @chat.messages
     end
 
     def create
@@ -24,12 +17,6 @@ module Messenger
         @chat.chat_members.create(user_id: current_user.id)
         @chat.chat_members.create(user_id: params[:user_id])
       end
-    end
-
-    private
-
-    def set_chat
-      @chat = Chat.find(params[:id])
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 Messenger::Engine.routes.draw do
   devise_for :users, path: 'users', class_name: 'Messenger::User', module: :devise
 
   root to: 'chats#index'
-  resources :chats, only: [:index, :show, :create]
-  resources :messages, only: :create
+  resources :chats, only: %i[index create] do
+    resources :messages, only: %i[index create]
+  end
 end
