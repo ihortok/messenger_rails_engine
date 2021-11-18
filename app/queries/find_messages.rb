c522d0bbda3e8e65e098155e351cd4b11cf4386a
ihortok <itokaryk@gmail.com>
2021-11-18 03:24:24 +0200

    search

---
+++ app/queries/find_messages.rb
@@ -0,0 +1,30 @@
+class FindMessages
+  attr_accessor :initial_scope
+
+  def initialize(initial_scope)
+    @initial_scope = initial_scope.order(created_at: :desc)
+  end
+
+  def call(params = {})
+    return initial_scope unless params[:search]
+
+    query = params[:search].split
+
+    initial_scope.search(
+      {
+        query: {
+          bool: {
+            must: [
+              {
+                multi_match: {
+                  query: params[:search],
+                  fields: [:content]
+                }
+              }
+            ]
+          }
+        }
+      }
+    )
+  end
+end
--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
 require 'pagy'
+require 'pagy/extras/elasticsearch_rails'
